0099eeb2fbbb8c77e2b8bbdb6466d83728cd62ee
kevinlaiGH <kevin.km.lai@gmail.com>
2023-02-22 11:26:44 +1100

    Display product image, name and price completed

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,9 +4,15 @@ export const Card = ({ product }) => {
   const { productName, productImage, price } = product;
   return (
     <div className="grid-item">
-      {productImage}
-      {productName}
-      {price}
+        <div className="card">
+            <div className="card-image">
+                <img src={`https://loremflickr.com/320/240/${productName}`} alt={productImage} />
+            </div>
+            <div className="card-content">
+                <h2>{productName}</h2>
+                <p>{price}</p>
+            </div>
+        </div>
     </div>
   );
 };
